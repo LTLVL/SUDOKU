c3713ed3467d2acc54fb9b048742e789cacd10da
Liu <1184690200@qq.com>
2023-10-18 18:24:13 +0800

    【描述】完成开发

--- src/main/java/org/example/Sudoku.java
+++ src/main/java/org/example/Sudoku.java
@@ -1,4 +1,5 @@
 package org.example;
+
 import org.apache.commons.lang3.SerializationUtils;
 
 import java.util.*;
@@ -9,7 +10,7 @@ import java.util.*;
  */
 public class Sudoku {
     private static final int COUNT = 9; //9*9数独
-    private static final ArrayList<Integer> list = new ArrayList<>() {{
+    private static final ArrayList<Integer> list = new ArrayList<>() {{ //标准
         add(1);
         add(2);
         add(3);
@@ -21,10 +22,12 @@ public class Sudoku {
         add(9);
     }};
 
-    private static ArrayList<int[][]> result = new ArrayList<>();
+    private static int[][] result = new int[COUNT][COUNT]; //数独结果
 
+    private static boolean found = false; //是否找到数独
 
     public static void main(String[] args) {
+        long start = System.currentTimeMillis();
         ArrayList<ArrayList<Integer>> lists = new ArrayList<>() {{
             for (int i = 0; i < COUNT; i++) {
                 add(new ArrayList<>(list));
@@ -32,6 +35,16 @@ public class Sudoku {
         }};
         int[][] sudoku = HandleInput(lists);
         Process(sudoku, 0, 0, lists);
+        if (found) {
+            for (int[] ints : result) {
+                for (int j = 0; j < COUNT; j++) {
+                    System.out.print(ints[j] + " ");
+                }
+                System.out.println();
+            }
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("总耗时：" + (end - start) + "ms");
         return;
     }
 
@@ -58,15 +71,31 @@ public class Sudoku {
         return sudoku;
     }
 
+    /**
+     * 回溯算法处理数独主流程
+     *
+     * @param sudoku
+     * @param i
+     * @param j
+     * @param lists
+     */
     public static void Process(int[][] sudoku, int i, int j, ArrayList<ArrayList<Integer>> lists) {
+        if (found) {
+            return;
+        }
         if (i == COUNT) {
-            if (Check(sudoku)) {
-                result.add(sudoku);
+            if (Check(sudoku, COUNT - 1, COUNT - 1)) {
+                found = true;
+                result = sudoku;
             }
             return;
         }
-        if (sudoku[i][j] != 0) { //该位置已被指定则跳过
-            Process(sudoku, j + 1 < COUNT ? i : i + 1, j + 1 < COUNT ? j + 1 : 0, lists);
+        if (sudoku[i][j] != 0) { //该位置已被提前指定则跳过
+            if (Check(sudoku, i, j)) {
+                Process(sudoku, j + 1 < COUNT ? i : i + 1, j + 1 < COUNT ? j + 1 : 0, lists);
+            } else {
+                return;
+            }
         }
         for (int k = 1; k <= COUNT; k++) {
             if (!lists.get(i).contains(k)) { //该数字在该行已被占用则换下一个数字
@@ -77,7 +106,9 @@ public class Sudoku {
             int[][] tempSudoku = CopyArray(sudoku);
             tempList.get(i).remove((Integer) k);
             tempSudoku[i][j] = k;
-            Process(tempSudoku, j + 1 < COUNT ? i : i + 1, j + 1 < COUNT ? j + 1 : 0, tempList);
+            if (i < 1 || Check(tempSudoku, i, j)) {
+                Process(tempSudoku, j + 1 < COUNT ? i : i + 1, j + 1 < COUNT ? j + 1 : 0, tempList);
+            }
         }
     }
 
@@ -100,35 +131,47 @@ public class Sudoku {
      *
      * @return boolean
      */
-    public static boolean Check(int[][] sudoku) {
+    public static boolean Check(int[][] sudoku, int Row, int Column) {
+        HashSet<Integer> set = new HashSet<>();
         for (int i = 0; i < COUNT; i++) { //按列检查
-            HashSet<Integer> set = new HashSet<>(list);
-            for (int j = 0; j < COUNT; j++) {
-                if (sudoku[j][i] == 0) {
-                    continue;
-                }
-                if (set.contains(sudoku[j][i])) {
-                    set.remove(sudoku[j][i]);
-                    continue;
-                }
+            if (sudoku[i][Column] == 0) {
+                break;
+            }
+            if (set.contains(sudoku[i][Column])) {
                 return false;
             }
+            set.add(sudoku[i][Column]);
         }
+        return CheckByBlock(sudoku, (Row / 3) * 3, (Row / 3) * 3 + 3
+                , (Column / 3) * 3, (Column / 3) * 3 + 3);
+    }
 
-//        for (int i = 0; i < COUNT; i++){ //按块检查
-//            HashSet<Integer> set = new HashSet<>(list);
-//            for (int j = 0; j < COUNT; j++) {
-//                if(sudoku[i][j] == 0){
-//                    continue;
-//                }
-//                if(set.contains(sudoku[i][j])){
-//                    set.remove(sudoku[i][j]);
-//                    continue;
-//                }
-//                return false;
-//            }
-//        }
-
+    /**
+     * 按块检查
+     *
+     * @param sudoku
+     * @param minR
+     * @param maxR
+     * @param minC
+     * @param maxC
+     * @return boolean
+     */
+    public static boolean CheckByBlock(int[][] sudoku, int minR, int maxR, int minC, int maxC) {
+        if (minR == COUNT) {
+            return true;
+        }
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = minR; i < maxR; i++) { //按块检查
+            for (int j = minC; j < maxC; j++) {
+                if (sudoku[i][j] == 0) {
+                    return true;
+                }
+                if (set.contains(sudoku[i][j])) {
+                    return false;
+                }
+                set.add(sudoku[i][j]);
+            }
+        }
         return true;
     }
 }
